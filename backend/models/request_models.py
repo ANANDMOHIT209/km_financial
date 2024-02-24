f847d5f1687a5d437523cfa61b65ef52481aa86c
ANANDMOHIT209 <anandmohit852@gmail.com>
2024-02-24 11:24:18 +0530

    ADDED new file

---
+++ backend/models/request_models.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import List, Optional, Union, Dict
+import pydantic
+
+from pydantic import BaseModel
+
+class ClientReq(BaseModel):
+    source: Union[str, None] = None
+    ttl: Optional[int] = None
+
+class UserDetails(ClientReq):
+    username: str
+    email: str
+    password: Optional[str] = None
